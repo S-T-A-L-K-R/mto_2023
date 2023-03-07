943ce81e3181741aa6f6676973ee6ca694b4a166
S-T-A-L-K-R <name.generate@gmail.com>
2023-03-07 18:46:43 +0100

    middle of...

--- lab2/main.py
+++ lab2/main.py
@@ -6,40 +6,45 @@ def my_printf(format_string, param):
     #print(format_string)
     # print(param)
     shouldDo = True
+    skip = 0
     flag = True
+    param_min_size = 0
     for i in range(0, len(format_string)):
-        if shouldDo:
+        if skip == 0:
             # if format_string[i] == '#' and format_string[i+1] == 'k':
             if format_string[i] == '#':
                 k = 1
-                param_min_size = 0
                 while(True):
                     if format_string[i+k].isnumeric():
                         param_min_size = param_min_size * 10 + int(format_string[i+k])
                         k+=1
                     elif format_string[i+k] == "k":
+                        flag = True
                         break
                     else:
                         flag = False
+                        shouldDo = False
                         break
                 if flag == True:
-                    pass
+                    if len(param) < param_min_size:
+                        for k in range(0, param_min_size - len(param)):
+                            print(" ", end="")
+                    for j in range(0, len(param)):
+                        if param[j].islower():
+                            print(param[j].upper(), end="")
+                        else:
+                            print(param[j].lower(), end="")
+                    # print(param, end="")
+                    skip = k
                 else:
-                    pass
-                for j in range(0, len(param)):
-                    if param[j].islower():
-                        print(param[j].upper(), end="")
-                    else:
-                        print(param[j].lower(), end="")
-                # print(param, end="")
-                shouldDo=False
+                    print(format_string[i],end="")
             else:
                 if format_string[i].islower():
                     print(format_string[i].upper(), end="")
                 else:
                     print(format_string[i].lower(), end="")
         else:
-            shouldDo=True
+            skip -= 1
     print("")
 
 data = sys.stdin.readlines()
