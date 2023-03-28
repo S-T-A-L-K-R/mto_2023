c27429dcb531f9d08c5d8711943cbbe3be81662c
Hubert <name.generate@gmail.com>
2023-03-28 15:16:01 +0000

    code formatting

--- lab4/main.py
+++ lab4/main.py
@@ -2,18 +2,18 @@
 
 import sys
 
-def my_printf(format_string,param):
+def my_printf(format_string, param):
     #print(format_string)
-    shouldDo=True
+    shouldDo = True
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
+            if format_string[i] == '#' and format_string[i+1] == 'g':
+                print(param, end="")
+                shouldDo = False
             else:
-                print(format_string[idx],end="")
+                print(format_string[i],end="")
         else:
-            shouldDo=True
+            shouldDo = True
     print("")
 
 data=sys.stdin.readlines()
