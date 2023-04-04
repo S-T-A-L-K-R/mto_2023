c426ae5e37a4a845a90295e257a7a448f3bf57f0
Hubert <name.generate@gmail.com>
2023-04-04 15:39:15 +0000

    first look at "main"

--- lab5/main.py
+++ lab5/main.py
@@ -2,13 +2,22 @@
 
 import sys
 
+def modify_param(param):
+    return param
+
 def my_printf(format_string, param):
     #print(format_string)
     shouldDo = True
     for i in range(0, len(format_string)):
         if shouldDo:
-            if format_string[i] == '#' and format_string[i+1] == 'g':
-                param = str(param)[::-1]
+            # if format_string[i] == '#' and format_string[i+1] == 'g':
+            if format_string[i] == '#':
+                g_index = format_string.find("g", i)
+                if g_index > i:
+                    x = int(param[i:g_index])
+                    param = modify_param(param)
+                    if x > len(param):
+                        print(" " * (x - len(param)))
                 print(param, end="")
                 shouldDo = False
             else:
