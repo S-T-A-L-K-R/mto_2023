2ffe99bdd66263b087a6257c29e52444c6682e52
Hubert <name.generate@gmail.com>
2023-04-04 15:10:57 +0000

    lab4 => lab5

---
+++ lab5/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+
+def my_printf(format_string, param):
+    #print(format_string)
+    shouldDo = True
+    for i in range(0, len(format_string)):
+        if shouldDo:
+            if format_string[i] == '#' and format_string[i+1] == 'g':
+                param = str(param)[::-1]
+                print(param, end="")
+                shouldDo = False
+            else:
+                print(format_string[i],end="")
+        else:
+            shouldDo = True
+    print("")
+
+data=sys.stdin.readlines()
+
+for i in range(0,len(data),2):
+    my_printf(data[i].rstrip(),data[i+1].rstrip())
