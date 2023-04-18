f704c77c7619882298ec1c49bb8ac89a93048823
Hubert <name.generate@gmail.com>
2023-04-18 15:25:14 +0000

    lab3 => lab6

--- lab6/main.py
+++ lab6/main.py
@@ -14,32 +14,79 @@ def modify_param(param):
 
 def my_printf(format_string, param):
     #print(format_string)
+    # print(param)
     shouldDo = True
-    g_index = 0
+    skip = 0
+    flag = True
+    param_min_size = 0
     for i in range(0, len(format_string)):
-        if shouldDo and i >= g_index:
-            # if format_string[i] == '#' and format_string[i+1] == 'g':
-            if format_string[i] == '#':
-                g = format_string.find("g", i)
-                if g > (i + 1):
-                    x = format_string[i+1 : g]
-                    if x.isdigit():
-                        x = int(x)
-                        param = modify_param(param)
-                        if x > len(param):
-                            print(" " * (x - len(param)), end="")
-                        g_index = g + 1
-                        print(param, end="")
-                        shouldDo = False
+        if skip == 0:
+            if format_string[i] == '#' and format_string[i+1] == '.':
+                param_len = 1
+                while(True):
+                    if format_string[i+1+param_len].isnumeric():
+                        param_min_size = param_min_size * 10 + int(format_string[i+1+param_len])
+                        param_len+=1
+                    elif format_string[i+1+param_len] == "k":
+                        flag = True
+                        break
                     else:
-                        print(format_string[i],end="")
+                        flag = False
+                        shouldDo = False
+                        break
+                if flag == True:
+                    if len(param) < param_min_size:
+                        for j in range(0, len(param)):
+                            if param[j].islower():
+                                print(param[j].upper(), end="")
+                            else:
+                                print(param[j].lower(), end="")
+                    else:
+                        for j in range(0, param_min_size):
+                            if param[j].islower():
+                                print(param[j].upper(), end="")
+                            else:
+                                print(param[j].lower(), end="")
+                    # print(param, end="")
+                    skip = param_len + 1
+                else:
+                    print(format_string[i],end="")
+            
+            elif format_string[i] == '#':
+                param_len = 1
+                while(True):
+                    if format_string[i+param_len].isnumeric():
+                        param_min_size = param_min_size * 10 + int(format_string[i+param_len])
+                        param_len+=1
+                    elif format_string[i+param_len] == "k":
+                        flag = True
+                        break
+                    else:
+                        flag = False
+                        shouldDo = False
+                        break
+                if flag == True:
+                    if len(param) < param_min_size:
+                        for k in range(0, param_min_size - len(param)):
+                            print(" ", end="")
+                    for j in range(0, len(param)):
+                        if param[j].islower():
+                            print(param[j].upper(), end="")
+                        else:
+                            print(param[j].lower(), end="")
+                    # print(param, end="")
+                    skip = param_len
                 else:
                     print(format_string[i],end="")
             else:
-                print(format_string[i],end="")
+                if format_string[i].islower():
+                    print(format_string[i].upper(), end="")
+                else:
+                    print(format_string[i].lower(), end="")
         else:
-            shouldDo = True
+            skip -= 1
     print("")
+
 
 data=sys.stdin.readlines()
 
